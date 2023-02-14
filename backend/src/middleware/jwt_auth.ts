73898e5c899016ddd82cf3755c0f68e9f0eef4fa
dqhieu9301 <dinhhieu9301@gmail.com>
2023-02-14 20:28:09 +0700

    buid middware auth

---
+++ backend/src/middleware/jwt_auth.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, {Request, Response} from 'express';
+import { NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+
+function verifyToken (req: Request, res: Response , next: NextFunction) {
+  const authorizationHeader = req.headers['authorization'];
+  if(!authorizationHeader) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized!",
+    });
+  }
+  const token = authorizationHeader.split(' ')[1];
+  jwt.verify(token, process.env.SECRET as string, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized!",
+      });
+    }
+    next();
+  });
+}
+
+export { verifyToken };
